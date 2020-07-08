0c5c84ca73b019352e79dd87ea01fac92458c399
Bekinston <bektalgat1@gmail.com>
2020-07-08 17:41:00 +0600

    Updates

---
+++ precache-manifest.68df31b1c49aff01163e046952a18809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70029bf381636865f4c",
+    "url": "/b1.github.io/static/js/main.e11bce8e.chunk.js"
+  },
+  {
+    "revision": "c28b43b58dd1876f7131",
+    "url": "/b1.github.io/static/js/runtime~main.f1a6578b.js"
+  },
+  {
+    "revision": "61c954d3419518d60ecd",
+    "url": "/b1.github.io/static/css/2.09e8577b.chunk.css"
+  },
+  {
+    "revision": "61c954d3419518d60ecd",
+    "url": "/b1.github.io/static/js/2.a4cc1c87.chunk.js"
+  },
+  {
+    "revision": "3d9c7c0d60305c31ba11b91ced358cd5",
+    "url": "/b1.github.io/index.html"
+  }
+];
