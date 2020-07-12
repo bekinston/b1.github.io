8fb5a2d6672503524807dab46e523075e05846c1
Bekinston <bektalgat1@gmail.com>
2020-07-13 05:07:31 +0600

    1

--- basic/src/LeftSideBar.js
+++ basic/src/LeftSideBar.js
@@ -56,9 +56,9 @@ class LeftSideBar extends React.Component{
     let boxes = [];
     if(this.props.is_map_ready){
       boxes = [
-        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Инфраструктуры"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="info" displayRightBox={this.props.displayRightBox}/>,
-        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Месторождения" map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="minning" displayRightBox={this.props.displayRightBox}/>,
-        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Производства"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="production" displayRightBox={this.props.displayRightBox}/>,
+        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Инфраструктура"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="info" displayRightBox={this.props.displayRightBox}/>,
+        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Месторождение" map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="minning" displayRightBox={this.props.displayRightBox}/>,
+        <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Производство"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="production" displayRightBox={this.props.displayRightBox}/>,
         <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Энергетика"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="energy" displayRightBox={this.props.displayRightBox}/>,
         <LeftCheckBox layers_id={this.props.layers_id} update_layers={this.props.update_layers} symbol="Переработка"  map={this.props.map} onLayerDisable={this.props.onLayerDisable} category="processing" displayRightBox={this.props.displayRightBox}/>]
     }
